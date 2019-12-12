b76ef455e10f3c910755cf39ddc5adc4ac856fd1
yjin8 <=>
2019-12-12 02:17:13 -0500

    added comment

--- cus715_final_project/newtons_iteration.py
+++ cus715_final_project/newtons_iteration.py
@@ -1,22 +1,69 @@
 from scipy.misc import derivative
+from math import ceil
+import numpy as np
 
-
-def newton(func, estimate, max_iterations=100, max_error=.00001):
-    if max_iterations == 0:
-        return estimate
-    else:
-        new_estimate = \
-            max_iterations + func(estimate) / derivative(func, estimate)
-        return newton(
-            func,
-            new_estimate,
-            max_iterations=max_iterations-1,
-            max_error=max_error)
+# basic newton iteration algorithm
+def newton(func, estimate, min_error=.0001):
+    diff = func(estimate)/derivative(func, estimate)
+    while abs(diff) > min_error:
+        diff = func(estimate)/derivative(func, estimate)
+        estimate = estimate - diff
+        diff = func(estimate)/derivative(func, estimate)
+    return estimate
 
 
 def f(x):
     return x**5 - x - 7
+def y(x):
+    return x**5 - 1000
+
+'''
+def InvertSeries(ma):
+    n = len(ma)
+    if n==1:
+        return [-1/ma[0]]
+    k = -(-n//2) # ceil(n/2)
+    s = InvertSeries(ma[:k])+[0]*(n-k)
+    t = Mul(ma,s,n) # -a*s
+    t[0] += 1 # 1-a*s
+    return Add(s,Mul(s,t,n))# s+s(1-a*s)
+def NewtonInvert(a):
+    return InvertSeries(ScalarMul(-1,a))
+
+'''
+def invert_series(ma):
+    n = len(ma)
+    if n == 1:
+        return [-1/ma[0]]
+    k = -(-n//2) # int(ceil(n / 2))
+    s = invert_series(ma[:k])+[0]*(n-k)
+    # t = np.multiply(s, ma)
+    t = Mul(ma, s, n)  # -a*s
+    t[0] += 1  # 1-a*s
+    return Add(s, Mul(s, t, n))  # s+s(1-a*s)
+
+
+def newton_invert(a):
+    return invert_series(np.multiply(-1, a))
+
+
+def Mul(to_mult, lst, pos):
+    lst[pos] = to_mult
+
+
+def Add(arg1, arg2):
+    return [arg1, arg2]
+
+
+def ScalarMul(to_mult, matrix):
+    return np.multiply(to_mult, matrix)
+    # return [i*to_mult for i in lst]
 
 
 if __name__ == '__main__':
-    print(newton(f, 2))
+    print("Estimating root of x^5 - x - 7: " + str(newton(f, 2.5)))
+    # outputs 1.53548522912
+    print("Estimating root of x^5 - 1000 or 1000^(1/5): " + str(newton(y, 3.5)))
+    # outputs 3.98107170553
+    # print(ScalarMul(-1, [1,2,4]))
+    print(newton_invert([1,2,3]))
